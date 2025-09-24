a91cef6bc113138a69cbf7d278205479ba509e22
James Robinson <jpwrobinson@gmail.com>
2025-09-24 11:08:50 +0200

    biplot

---
+++ scripts/pmii_vs_consumption.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(countrycode)
+theme_set(theme_bw())
+
+pmii<-readRDS('data/inadequate_intakes/mean_national_inadequate_intakes_of_fish_nutrients.Rds')
+gdd<-read.csv('data/gdd_seafood_intake_country.csv')
+
+gdd<-gdd %>% left_join(pmii) %>% 
+  mutate(country = countrycode(iso3, 'iso3c', 'country.name.en'))
+
+meds<-gdd %>% summarise(median = median(median), pdeficient = median(pdeficient, na.rm=TRUE))
+
+ggplot(gdd, aes(median, pdeficient)) + 
+  geom_vline(xintercept = meds[1] %>% pull, col='red', alpha=0.5) +
+  geom_hline(yintercept = meds[2] %>% pull, col='red', alpha=0.5) +
+  geom_point() +
+  ggrepel::geom_text_repel(aes(label = country), size=2) +
+  labs(y = 'prevalence inadequate intake', x = 'Seafood consumption, g/day')
