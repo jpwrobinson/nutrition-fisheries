cbb40c2403616f1acbac793a70ad5d9d8d6d723f
James Robinson <jpwrobinson@gmail.com>
2025-09-25 13:01:53 +0200

    ai and pdf

--- scripts/pmii_vs_consumption.R
+++ scripts/pmii_vs_consumption.R
@@ -10,9 +10,13 @@ gdd<-gdd %>% left_join(pmii) %>%
 
 meds<-gdd %>% summarise(median = median(median), pdeficient = median(pdeficient, na.rm=TRUE))
 
-ggplot(gdd, aes(median, pdeficient)) + 
+g1<-ggplot(gdd, aes(median, pdeficient)) + 
   geom_vline(xintercept = meds[1] %>% pull, col='red', alpha=0.5) +
   geom_hline(yintercept = meds[2] %>% pull, col='red', alpha=0.5) +
   geom_point() +
   ggrepel::geom_text_repel(aes(label = country), size=2) +
   labs(y = 'prevalence inadequate intake', x = 'Seafood consumption, g/day')
+
+pdf(file = 'pmii_vs_consumption.pdf', height=4, width=7)
+print(g1)
+dev.off()
